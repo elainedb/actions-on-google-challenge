099bef357cec0f172151ea2d7cd94004b561961d
Elaine Dias Batista <diasbatista.e@sfeir.com>
2017-08-03 21:43:50 +0200

    add deploy.sh

---
+++ functions/deploy.sh
@@ -0,0 +1,2 @@
+firebase deploy --only functions
+curl -X POST -H 'Content-type: application/json' --data '{"text":"someone just deployed!"}' https://hooks.slack.com/services/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
