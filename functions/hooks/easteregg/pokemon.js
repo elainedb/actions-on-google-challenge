62ca1e6ec469c677a43e86340013616c25fd3085
sylvia <skhuu@meetphone.com>
2017-08-06 19:01:39 +0200

    shorter sentences

--- functions/hooks/easteregg/pokemon.js
+++ functions/hooks/easteregg/pokemon.js
@@ -82,78 +82,79 @@ const ENTITY_DONPHAN = "donphan";
 
 // pokemon answers
 
-const SENTENCE_PIKACHU = "pikachu is the most famous pokemon in the world and is Ash's best loyal friend ! pika pika forever !";
-const SENTENCE_PICHU = "Pichu is a social Pokémon known for its playful and mischievous demeanor. It is usually found in groups and often touch tails with other Pichu as a show of courage, creating a shower of sparks that can make them cry.";
-const SENTENCE_RAICHU = "raichu is the third evolution of pichu. he evolves when he is exposed to a thunder stone. By collecting its psychic power in its tail, it is able to ride it like a surfboard and float in the air. While its cheek sacs are still used to store electricity, rubbing them releases a sweet aroma.";
-const SENTENCE_RATTATA = "rattata is the most common pokemon on earth with his friend with wings. It is the only possible starter Pokémon in Pokémon Rumble.";
-const SENTENCE_RATICATE = "Raticate is selective in what it eats, however, only eating fresh, high quality foods. Rumor has it that a certain high-class restaurant even exploits this selectivity, bringing Raticate along to buy ingredients and letting it taste test new dishes.";
-const SENTENCE_MARILL = "marill is a kind of little water mouse with a blue bubble-like ball at the tip of its zigzagging black tail full of oil. The oil is lighter than water, and keeps Marill afloat in the water.";
-const SENTENCE_AZUMARILL = "Azumarill has highly developed hearing that allows it to hear distances, even when it is underwater. By keeping still and listening closely, it can identity prey even in wild, fast-moving rivers. If Azumarill spots a drowning Pokémon, it will make a balloon out of air.";
+const SENTENCE_PIKACHU = "pika pika ! Unfortunally pikachu is road tripping around the world with Ash and can not be there, let's try one more time !";
+const SENTENCE_PICHU = "Pichu has been grounded and is actually having a bath with his mom and so it's not the expected answer, let's play again !";
+const SENTENCE_RAICHU = "raichu can not talk because he is providing electricity to his little brother to warm his bath, so let's try again !";
 
-const SENTENCE_MAGIKARP = "Magikarp is the utmost lovely and useless pokemon of the game, chocking that he become so powerfull after his transformation into Gyarados starting at level 20 !";
+const SENTENCE_RATTATA = "rattata is dreaming of trying to fly with his winged friend instead of collecting raspberry for his father. So no, it is not him !";
+const SENTENCE_RATICATE = "Raticate is in the kitchen awaiting for raspberry, trying to create a new dishes to his wife for their wedding anniversary, so it's not him !";
 
-const SENTENCE_BULBASAUR = "Bulbasaur is one of three starter Pokémon of Kanto. He is a dual-type Grass/Poison Pokémon introduced in Generation one. Many Bulbasaur gather every year in a hidden garden in Kanto to evolve into Ivysaur in a ceremony led by a Venusaur.";
-const SENTENCE_SQUIRTLE = "Squirtle is one of three starter Pokémon of Kanto. Some of them joined the Squirtle Squad after being abandoned by their trainers. They used to run wild in a small town in Kanto stealing things, writing graffiti and causing trouble for travelers in the area.";
-const SENTENCE_CHARMANDER = "Charmander is a Fire-type Pokémon, he is one of three starter Pokémon of Kanto. A fire burns at the tip of his slender tail. The flame can be used as an indication of Charmander's health and mood, burning brightly when the Pokémon is strong, weakly when it is exhausted, wavering when it is happy, and blazing when it is enraged.";
-const SENTENCE_CHARIZARD = "Charizard is a dual-type Fire/Flying Pokémon. It is the final form of Charmander. It can Mega Evolve into two forms with specials items. Its fiery breath is capable of melting boulders, massive glaciers, and has been known to accidentally cause forest fires.";
+const SENTENCE_MARILL = "marill dabbles with his brothers and sisters in the neighboring lake in secret instead of doing his homework... so it can not be him !";
+const SENTENCE_AZUMARILL = "Azumarill is seeking his children, wondering where they are instead of doing theirs homeworks. try an other answer !";
 
-const SENTENCE_PIDGEY = "Pidgey is the most common pokemon on earth with his large teeth friend. It is a docile Pokémon, and generally prefers to flee from its enemies rather than fight them.";
-const SENTENCE_PIDGEOTTO = "pidgetto is pidgey evolution and start at level 18. He is a powerful flier capable of carrying prey several miles to its nest. It has amazing vision,";
-const SENTENCE_PIDGEOT = "pidgeot is the third evolution of pidgey and start at level 36. This Pokémon is capable of flying faster than the speed of sound. Its plumage is often described as beautiful.";
-const SENTENCE_PSYDUCK = "psyduck is a water type poekmon. This poor one is constantly stunned by its headache and is unable to think very clearly.";
-const SENTENCE_GOLDUCK = "golduck evolves from Psyduck starting at level thirty three and has the ability to give knowledge to its Trainer and fellow Pokémon through telepathy.";
-const SENTENCE_MURKROW = "Murkrow has a fondness for anything sparkly, and it will steal things such as jewelry to bring it back to its nest. Its fondness for shiny objects go as far as fighting with other Pokémon that enjoy shiny objects to gain their stashes.";
-const SENTENCE_HOOTHOOT = "Inside Hoothoot's body lies a special organ that senses Earth's rotation, allowing it to keep track of time. Every day at exactly the same time, a Hoothoot will begin hooting.";
-const SENTENCE_NOCTOWL = "Noctowl is a nocturnal Pokémon. It has exceptional hearing and eyesight, can fly silently, and can turn its heads at a 180° angle to increase its intellect.";
+const SENTENCE_MAGIKARP = "Magikarp, my favorite useless pokemon of the world... but now he is too busy to find water to scream. try another answer !";
 
-const SENTENCE_ARTICUNO = "articuno is a dual-type Ice/Flying Legendary Pokémon, one of the three Legendary birds of Kanto. according to legend, it appears before doomed travelers lost in icy regions";
-const SENTENCE_MOLTRES = "moltres is a dual-type Fire/Flying Legendary Pokémon, one of the three Legendary birds of Kanto. By dipping itself into the magma of an active volcano, this Pokémon can heal itself. It migrates to the south with the coming of spring, and is said to bring an early springtime to cold lands.";
-const SENTENCE_ZAPDOS = "zapdos is a dual-type Electric/Flying Legendary Pokémon, one of the three Legendary birds of Kanto. When stricken by lightning, it gains power. Zapdos reportedly appears only during thunderstorms, and is said to live among thunderclouds.";
+const SENTENCE_BULBASAUR = "Bulbasaur is gardening at the bottom of a garden with a friend and it seems that something goes wrong. try another answer !";
+const SENTENCE_SQUIRTLE = "Squirtle is not here, he has joined the Squirtle Squad and used to run wild. He is probably writing graffiti and and swaggering with his bro at this time. let's try again !";
+const SENTENCE_CHARMANDER = "Charmander is with his friend at the bottom of the garden... it smells that they are starting a barbecue or something like that, so let's try another answer !";
+const SENTENCE_CHARIZARD = "Right, he is actually screaming at the bottom of the garden, I don't know why. But Charizard doesn't sounds like that, so let's try again !";
 
-const SENTENCE_MEOWTH = "Meowth is one of the two main Pokémon in the series. He is one part of the team rocket with jesse and james";
-const SENTENCE_PERSIAN = "persian is a feline normal type pokemon evolves from meowth at level 28. He is popular for its elegance, it is extremely difficult to raise as a pet due to its fickle and temperamental personality.";
-const SENTENCE_MEW = "mew is a Psychic-type Mythical Pokémon incredibly adaptable, able to travel freely in the air or underwater.";
-const SENTENCE_MEWTWO = "mewtwo is a Psychic-type Legendary Pokémon and was created after years of horrific gene splicing and DNA engineering experiments based on Mew. It is said to have the most savage heart among all Pokémon but it was later shown to be caring, protective, and even altruistic.";
+const SENTENCE_PIDGEY = "Pidgey is trying desperately to awake his friend because if he doesn't bring raspberry to his daddy he will be grounded, so let's get another shot !";
+const SENTENCE_PIDGEOTTO = "pidgetto is having a laught watching his little neighbours set a fire to a wheelbarrow, so let's try again !";
+const SENTENCE_PIDGEOT = "pidgeot flew into his neighbor's garden for a wheelbarrow story... I don't know exactly but it can not be him, so let's play again !";
 
-const SENTENCE_GROWLITHE = "growlithe is a friendly and loyal Pokémon that will fearlessly defend its Trainer and territory from harm, even against larger, stronger enemies";
-const SENTENCE_ARCANINE = "Arcanine is a fiercely brave and loyal Pokémon. It is capable of running 6,200 miles in 24 hours with a speed and grace reminiscent of flight.";
-const SENTENCE_SNUBULL = "Snubbull possesses the ability to use keen sense of smell to track things down. Despite his fierce looking face, it is very playful and affectionate making it a popular companion among women.";
-const SENTENCE_HOUNDOUR = "Houndour is an intelligent Pokémon that forms packs to hunt for prey, and shows unparalleled teamwork. it will not abandon a sick pack member. ";
-const SENTENCE_HOUNDOOM = "In a Houndoom pack, the one with its horns raked sharply towards the back serves a leadership role. Its gut is full of toxins, which are used to create fire. This gives Houndoom's fire a foul odor and the wounds it inflicts will never heal.";
+const SENTENCE_PSYDUCK = "psyduck is missing, he escaped again from his owner and surely is trying to learn how to swim somewhere we don't know where, so let's try another answer !";
+const SENTENCE_GOLDUCK = "golduck is trying to talk to come back to psyduck through telepathy but it looks like that psyduck gave him a headache, so let's try again !";
+const SENTENCE_MURKROW = "Murkrow is actually grounded in his room, he tried to robbered a jewelry. This little boy has never been able to resist a brilliant object, so it is not him !";
 
-const SENTENCE_PONYTA = "Ponyta can control its flames so that its rider is not burned by them. Ponyta runs in fields and mountains all day";
-const SENTENCE_RAPIDASH = "rapidash runs at about 150 mph, chasing everything that moves in hopes of racing it.";
+const SENTENCE_HOOTHOOT = "Hoothoot has embarked on the music, he is currently rehearsing in his room. let's try again !";
+const SENTENCE_NOCTOWL = "Noctowl listens to hoothoot music through the door and is too busy to wonders if he should encourage or stop him. so let's play again !";
 
-const SENTENCE_EEVEE = "Eevee is said to have an irregularly shaped genetic structure that allows it to adapt to many different kinds of environments. Eventually, Eevee may evolve to better suit its surroundings.";
-const SENTENCE_VULPIX = "Vulpix has a flame inside its body that never goes out, and is capable of manipulating fire to such precision as to create floating wisps of flame.";
+const SENTENCE_ARTICUNO = "articuno has gone for a tour around the world with his two best friends to the delight of their fan so let's try again !";
+const SENTENCE_MOLTRES = "moltres has gone for a tour around the world with his two best friends to the delight of their fan, so it is not the correct answer";
+const SENTENCE_ZAPDOS = "zapdos has gone for a tour around the world with his two best friends to the delight of their fan so let's play again !";
 
-const SENTENCE_MAREEP = "Mareep's wool, which grows continuously, stores electricity, rubbing together and building a static charge. Touching the wool when it is charged will result in a static shock.";
-const SENTENCE_FLAAFY = "flaafy evolves from Mareep starting at level 15, Storing too much electricity caused Flaaffy to lose the wool over most of its body, but the bare, rubbery skin protects it from the electricity stored in its fleece.";
-const SENTENCE_AMPHAROS = "ampharos is the final form of Mareep. The orb on its tail can shine so bright, that it can be seen from space. People have used the light as a beacon, or to send signals across the ocean.";
+const SENTENCE_MEOWTH = "Meowth is in holiday camp trying to make new friend with 2 humans and other pokemon, let's try one more time ! ";
+const SENTENCE_PERSIAN = "persian is enjoying his free time without no one to bothered him, so it is not the good answer, let's try again !";
+const SENTENCE_MEW = "mew is daydreaming somewhere beetween water and air, we don't know when he is coming back home so it is not him, let's play again !";
+const SENTENCE_MEWTWO = "mewtwo is chasing after mew because mew is the only one who can calm his fierce heart, by bringing him to his origin, so let's try another one !";
+
+const SENTENCE_GROWLITHE = "growlithe has gone hiking for the first time with his brother and his friends, so it can not be him, let's try again !";
+const SENTENCE_ARCANINE = "Arcanine is leading a hike with friend and family all around the world for months, so let's play again !";
+const SENTENCE_SNUBULL = "Snubbull is shopping with his trainer to find a new little dress so it is not the good answer, let's try again";
+const SENTENCE_HOUNDOUR = "Houndour is gone for a hike with friend for a long time, he packed half the house afraid of missing something, so it's not the right answer, let's play again !";
+const SENTENCE_HOUNDOOM = "houndoom has gone with friends for a hike, afraid that his little brother missed something in his package and panic for nothing, so let's try another answer !";
+
+const SENTENCE_PONYTA = "Ponyta is running in fields and mountains all day, trying to be as fast as his big brother ! so it's not to good answer let's try again !";
+const SENTENCE_RAPIDASH = "rapidash is training his little brother to go faster, as he can run faster than the storm, so it cannot be him let's play again !";
+
+const SENTENCE_EEVEE = "Eevee is outside, trying to find his own personnality by finding what he likes or need with a help of a friend, so let's try again !";
+const SENTENCE_VULPIX = "Vulpix is outside, trying to convince a friend to become like him, so they can be brother of fire, so let's try again !";
+
+const SENTENCE_MAREEP = "Mareep is watching his big brother having his first shaving wool... he is too impressed to scream so let's try again !";
+const SENTENCE_FLAAFY = "flaafy is having his first lost of wool, trying to negociate to not loose everything, so it is not him let's play again !";
+const SENTENCE_AMPHAROS = "ampharos is too busy to shave his little brother and hold on to send signals across the ocean in the right way, so let's try one more time !";
 
 const SENTENCE_AIPOM = "Aipom is a monkey-like Pokémon with a three-fingered hand at the end of its tail. The tail is strong enough to anchor Aipom to a branch and suspend the rest of its body in the air, but also delicate enough to pick fruit and manipulate objects";
-const SENTENCE_MANKEY = "Mankey's preferred food is chestnuts, although Mankey usually lacks the dexterity to peel them. Mankey will get agitated and rage after attempting to peel a chestnut's spiky shell. Additionally, it also appears to be fond of bananas.";
+const SENTENCE_MANKEY = "Mankey is trying to peel a chestnut but I can hear him getting nervous, so let's try another answer !";
 const SENTENCE_PRIMEAPE = "When Primeape is furious, its blood circulation is boosted. This makes its muscles stronger, but at the cost of some of its intelligence. It has shown some fondness for bananas.";
 
-const SENTENCE_EKANS = "ekans is jesse companion received as a chūgen gift, By flicking its tongue out, it is able to detect danger and the scent of prey. Its jaw can detach to swallow large prey, but such a meal can make Ekans too heavy to move.";
-const SENTENCE_ARBOK = "Arbok is territorial. It lashes out at intruders with long fangs tipped with deadly venom. With a vengeful nature, it will not give up a chase once it targets its prey or an opponent, no matter how far.";
+const SENTENCE_EKANS = "ekans is in holiday camp, making friend with other pokemon and some humans, apparently they are starting a band, so let's try one more time !";
+const SENTENCE_ARBOK = "Arbok is too territorial, he went to look for hikers who would walk on his land, so it is not the right answer let's play again !";
 
-const SENTENCE_TAUROS = "Tauros is a violent, rowdy Pokémon that whips itself into a frenzy using its three tails. Once it starts charging, it is unable to stop or change course until it hits something.";
+const SENTENCE_TAUROS = "Tauros trains to be faster and more efficient with its 3 tails, so it's not him, let's try again !";
 
-const SENTENCE_POLITOED = "politoed evolves from Poliwhirl when traded holding a King's Rock. Politoed likes to expand its throat and sing. When three or more assemble, they sing in a chorus with a loud, bellowing voice.";
+const SENTENCE_POLITOED = "politoed has joined the King's Rock band and sing in a chorus with a loud, bellowing voice at the end of the world so it can not be him, let's try again !";
 
-const SENTENCE_GIRAFARIG = "Girafarig's tail possesses a head of its own incapable of independent thinking and relies on outside stimuli. However it is quite usefell because the head is able to warn the Pokémon of danger if it is sleeping.";
+const SENTENCE_GIRAFARIG = "Girafarig's tail's head is busy to calm down Mankey while Girafarig real head is picking bananas for the same reason, so let's try another one !";
 
-const SENTENCE_TEDDIURSA = "Teddiursa uses Beedrill pollen and fruit to create its own honey. It will hide food stores throughout its territory before food becomes scarce in winter.";
-const SENTENCE_URSARING = "Ursaring's sense of smell can distinguish any aroma, and as such it can find food buried deep underground or in streams.";
+const SENTENCE_TEDDIURSA = "Teddiursa is using Beedrill pollen and fruit to create its own honey for not becoming scarce this winter. it's not the correct answer let's try again !";
+const SENTENCE_URSARING = "Ursaring is helping Teddiursa making honey for winter, so it's not the good answer, let's play again !";
 
-const SENTENCE_SWINUB = "Swinub favorite food is a type of mushroom that grows beneath dead grass, though it also eats Berries. When a Swinub finds an attractive smell, it will run desperately to find out the source of the aroma.";
+const SENTENCE_SWINUB = "Swinub is gormandize somewhere in the wood with his favorite mushroom that grows beneath dead grass, so it's not the correct answer, let's try again !";
 
-const SENTENCE_PHANPY = "phanpy looks like a Dumbo's pokemon world ! To make its nest, it digs a vertical pit near the edge of a river.";
-const SENTENCE_DONPHAN = "donphan is the evolution of phanpy, chocking that the little one became so big and strong ! Donphan has the ability to curl up into a ball and violently roll around like a wheel. These rolling tackles are strong enough to knock down a house. ";
-
-// ??? maybe at end
-const SENTENCE_ALMOST = "it's not the correct answer but you are right, they are from an animal reign !"
+const SENTENCE_PHANPY = "phanpy waddles in the river playing with water and his trunk to the great displeasure of his papa, so let's try one more time !";
+const SENTENCE_DONPHAN = "donphan is trying to teach his son to curl up into a ball which seems more likely to play than learn, so let's try again !";
 
 class Pokemon extends SimpleIntent {
 
@@ -358,7 +359,10 @@ class Pokemon extends SimpleIntent {
 
                 default:
                 app.ask(SENTENCE_ALMOST)
+                break
         }
+
+
     }
 }
 
