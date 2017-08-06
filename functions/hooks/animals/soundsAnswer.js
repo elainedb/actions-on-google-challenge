7db380533c969827aaaac4ba3f0ac4fcdda46536
jadecdev <chabaro.j@sfeir-ext.com>
2017-08-06 16:01:01 +0200

    wip

--- functions/hooks/animals/soundsAnswer.js
+++ functions/hooks/animals/soundsAnswer.js
@@ -3,6 +3,7 @@
 
 const SimpleIntent = require('../shared/simpleIntent');
 const utils = require('../shared/_utils');
+const chooseGame = require('../chooseGame');
 
 const INTENT_ID = 'intent.auntie.game.animal';
 
@@ -18,10 +19,21 @@ class AnimalSoundsAnswer extends SimpleIntent {
     }
 
     trigger(app) {
-        app.setContext(CONTEXT_ANIMAL_SOUNDS, utils.DEFAULT_LIFESPAN, {});
         // if good answer
+        let resultMessage;
+        let context = app.getContext(CONTEXT_ANIMAL_SOUNDS);
+        console.log("context in sounds Answer");
+        console.log(context);
+        if (!context) {
+            app.setContext(CONTEXT_ANIMAL_SOUNDS, utils.DEFAULT_LIFESPAN, {
+                set_number : 0,
+                points : 0
+            });
+        }
+
         if (app.data.answer === app.getArgument(ENTITY_ANIMAL)) {
-            app.ask("yoohoo!");
+            context.parameters.points ++;
+
             if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
                 let richResponse = app.buildRichResponse()
                     .addSimpleResponse(`<speak>Good job! It was indeed a ${app.getArgument(ENTITY_ANIMAL)}</speak>`);
@@ -34,13 +46,37 @@ class AnimalSoundsAnswer extends SimpleIntent {
                     //     app.buildBasicCard(GAME_SUGGESTIONS[1])
                     //         .setImage(image[0], image[1])
                     //         .addButton('GOOOOOGLE_2', 'https://www.google.com/about/'))
-                app.ask(richResponse);
+                resultMessage = richResponse;
             } else {
-                app.ask(`<speak>Good job! It was indeed a ${app.getArgument(ENTITY_ANIMAL)}</speak>`);
+                resultMessage = `<speak>Good job! It was indeed a ${app.getArgument(ENTITY_ANIMAL)}</speak>`;
             }
         } else {
-            app.ask("too bad");
+
+            resultMessage = `Too bad`;
         }
+
+        app.setContext(CONTEXT_ANIMAL_SOUNDS, utils.DEFAULT_LIFESPAN, {
+            set_number : context.set_number++,
+            points : context.points
+        });
+        console.log("context after response");
+        console.log(app.getContext(CONTEXT_ANIMAL_SOUNDS));
+
+
+        // animal next question
+        let answers = app.data.animalAnswers ? new Set(app.data.animalAnswers) : chooseGame.ANIMALS;
+        if (answers.size === 0) {
+            app.data.animalAnswers = chooseGame.ANIMALS;
+        }
+        let question = utils.randomFromArray(chooseGame.SENTENCES_ANIMAL_SOUNDS);
+
+
+        app.ask(`${resultMessage}. 
+        You now have: 
+        ${app.getContext(CONTEXT_ANIMAL_SOUNDS).parameters.points}/${app.getContext(CONTEXT_ANIMAL_SOUNDS).parameters.set_number} 
+        good responses. 
+        Next!
+        ${question} ${utils.getRandomAnswer(app, answers, chooseGame.ANIMAL_SOUNDS_SRC)}`);
     }
 }
 
--- functions/hooks/chooseGame.js
+++ functions/hooks/chooseGame.js
@@ -5,13 +5,12 @@ const utils = require('./shared/_utils');
 
 const INTENT_ID = 'intent.auntie.choosegame';
 
-const DEFAULT_LIFESPAN = 5;
-
 const ENTITY_GAME = "game";
 
 // ANIMAL SOUNDS
 const ENTITY_GAME_ANIMAL_SOUNDS = "animal sounds";
 const CONTEXT_ANIMAL_SOUNDS = "context_game_animal";
+const CONTEXT_CHOOSE_GAME = "context_choose_game";
 
 const ENTITY_DOG = "dog";
 const ENTITY_CAT = "cat";
@@ -22,13 +21,18 @@ const ENTITY_LION = "lion";
 
 const ANIMALS = new Set([ENTITY_DOG, ENTITY_CAT, ENTITY_MONKEY, ENTITY_ELEPHANT, ENTITY_PIGEON, ENTITY_LION]);
 const ANIMAL_SOUNDS_SRC = new Set([
-                            'https://storage.googleapis.com/project-2252662783422070807.appspot.com/sounds/animals/dog/bark.mp3',
-                            'https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg',
-                            'http://www.freesoundeffects.com/mp3_466215.mp3',
-                            'http://www.freesoundeffects.com/mp3_466267.mp3',
-                            'http://www.freesoundeffects.com/mp3_466267.mp3',
-                            'http://www.freesoundeffects.com/mp3_466267.mp3'
-                        ]);
+    'https://storage.googleapis.com/project-2252662783422070807.appspot.com/sounds/animals/dog/bark.mp3',
+    'https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg',
+    'http://www.freesoundeffects.com/mp3_466215.mp3',
+    'http://www.freesoundeffects.com/mp3_466267.mp3',
+    'http://www.freesoundeffects.com/mp3_466267.mp3',
+    'http://www.freesoundeffects.com/mp3_466267.mp3'
+]);
+const SENTENCES_ANIMAL_SOUNDS = [
+    "OK, here we go! Do you know which animal makes this sound?",
+    "OK, let's play! Do you recognize this animal?",
+    "Yeah! Can you guess which animal makes this sound?"
+];
 
 // SING A SONG
 const ENTITY_GAME_SING_A_SONG = "sing a song";
@@ -42,11 +46,7 @@ const SENTENCES = [
     "Yeah! Let's play "
 ];
 
-const SENTENCES_ANIMAL_SOUNDS = [
-    "OK, here we go! Do you know which animal makes this sound?",
-    "OK, let's play! Do you recognize this animal?",
-    "Yeah! Can you guess which animal makes this sound?"
-];
+
 
 class ChooseGame extends SimpleIntent {
 
@@ -56,22 +56,31 @@ class ChooseGame extends SimpleIntent {
 
     trigger(app) {
         if (app.getArgument(ENTITY_GAME) === ENTITY_GAME_ANIMAL_SOUNDS) {
+            // question
+            let intro = utils.randomFromArray(SENTENCES) + app.getArgument(ENTITY_GAME) + ". ";
+            let question = utils.randomFromArray(SENTENCES_ANIMAL_SOUNDS);
+
+
             // possible answers
             let answers = app.data.animalAnswers ? new Set(app.data.animalAnswers) : ANIMALS;
             if (answers.size === 0) {
                 app.data.animalAnswers = ANIMALS;
             }
 
-            // question
-            let intro = utils.randomFromArray(SENTENCES) + app.getArgument(ENTITY_GAME) + ". ";
-            let question = utils.randomFromArray(SENTENCES_ANIMAL_SOUNDS);
-
-            app.setContext(CONTEXT_ANIMAL_SOUNDS, DEFAULT_LIFESPAN, {});
             app.ask(`<speak>${intro} ${question} ${utils.getRandomAnswer(app, answers, ANIMAL_SOUNDS_SRC)}</speak>`);
+
+            app.setContext(CONTEXT_ANIMAL_SOUNDS, utils.DEFAULT_LIFESPAN, {
+                set_number:0,
+                points:0
+            });
+
         } else {
-            app.ask('entity_game, you should not be here... you said ' + app.getArgument(ENTITY_GAME));
+            app.ask('entity_game, you should not be here if you want to play animal... you said ' + app.getArgument(ENTITY_GAME));
         }
     }
 }
 
 module.exports = ChooseGame;
+exports.ANIMALS = ANIMALS;
+exports.ANIMAL_SOUNDS_SRC = ANIMAL_SOUNDS_SRC;
+exports.SENTENCES_ANIMAL_SOUNDS = SENTENCES_ANIMAL_SOUNDS;
