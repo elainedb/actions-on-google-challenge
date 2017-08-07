368a22ac52251fbca9525d91a752b7b57791db85
Julien Duval <duval.j@sfeir.com>
2017-08-08 00:59:15 +0200

    The end ?? NOOO! More stories are coming from magical lands of ... to continue

--- functions/hooks/stories/storiesGame.js
+++ functions/hooks/stories/storiesGame.js
@@ -1,26 +1,31 @@
 'use strict';
 
-const SimpleIntent = require('../shared/simpleIntent');
 const utils = require('../shared/_utils');
+const SimpleIntent = require('../shared/simpleIntent');
 
-const INTENT_ID = 'input.game.story';
+const chooseGameData = require('../chooseGame/chooseGameData');
+
+const INTENT_ID = 'intent.auntie.game.story';
 
 const ARGS = {
     story: 'story',
     afterStoryChoice: 'choice'
 };
 
+const LIST_STORIES_SENTENCES = [
+    'Great! Here are the available stories: <p>numbers</p><p>towel</p>'
+];
+
 const STORIES = {
     numbers: 'What did 0 say to 8 ?<break time="3s"/>Nice belt!',
-    towels: 'What gets wetter the more it dries ?<break time="3s"/>A towel!'
+    towel: 'What gets wetter the more it dries ?<break time="3s"/>A towel!'
 };
 
 const CHOICES = {
     yes: 'yes',
+    again: 'again',
     no: 'no'
 };
-
-const REMOVE_STORY_WHEN_TOLD = true;
 
 class StoriesGame extends SimpleIntent {
 
@@ -32,41 +37,54 @@ class StoriesGame extends SimpleIntent {
     trigger(app) {
         const afterStoryResponse = app.getArgument(ARGS.afterStoryChoice);
         if (!afterStoryResponse) {
-            StoriesGame.tellAStory(app);
+            StoriesGame.chooseAndTellAStory(app, app.getArgument(ARGS.story));
         } else {
             StoriesGame.afterStory(app);
         }
     }
 
-    /** @param app {ApiAiApp} */
-    static tellAStory(app) {
+    /** @param app {ApiAiApp}
+     * @param storyCode {string|object=} */
+    static chooseAndTellAStory(app, storyCode) {
         let availableStories, pickedStory;
         if (app.data.stories && app.data.stories.length) {
             availableStories = app.data.stories.slice();
         } else {
-            availableStories = Object.values(STORIES).slice();
+            availableStories = Object.keys(STORIES).slice();
         }
 
-        const storyCode = app.getArgument(ARGS.story);
-        if (storyCode) {
-            pickedStory = STORIES[storyCode];
-        } else {
-            pickedStory = utils.randomFromArray(availableStories, REMOVE_STORY_WHEN_TOLD);
-        }
+        storyCode = storyCode || utils.randomFromArray(availableStories);
+        utils.removeFromArray(availableStories, storyCode);
+
+        pickedStory = STORIES[storyCode];
 
         app.ask(`<speak>${pickedStory}<break/> Do you want to ear another story ?</speak>`);
 
+        app.data.lastToldStory = storyCode;
         app.data.stories = availableStories;
     }
 
     /** @param app {ApiAiApp} */
     static afterStory(app) {
         const choice = app.getArgument(ARGS.afterStoryChoice);
-        if(choice === CHOICES.yes) {
-            StoriesGame.tellAStory(app);
-        } else if(choice === CHOICES.no) {
-            // TODO ask what to do. Remove context ?
+        switch (choice) {
+            case CHOICES.yes:
+                StoriesGame.chooseAndTellAStory(app);
+                break;
+            case CHOICES.again:
+                StoriesGame.chooseAndTellAStory(app, app.data.lastToldStory);
+                break;
+            case CHOICES.no:
+                let chooseGameResponse = utils.randomFromArray(chooseGameData.CHOOSE_GAME_SENTENCES);
+                app.ask(`<speak>Ok let's switch game. ${chooseGameResponse}</speak>`, chooseGameData.NO_INPUT_SUGGESTIONS);
+                break;
         }
+    }
+
+    /** @param app {ApiAiApp} */
+    static listStories(app) {
+        const text = utils.randomFromArray(LIST_STORIES_SENTENCES);
+        app.ask(`<speak>${text}</speak>`);
     }
 }
 
