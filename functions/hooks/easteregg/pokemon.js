7bbe0cfccd84d9851423b6921961007a873e67d4
Elaine Dias Batista <diasbatista.e@sfeir.com>
2017-08-13 16:26:47 +0200

    pokemon wording

--- functions/hooks/easteregg/pokemon.js
+++ functions/hooks/easteregg/pokemon.js
@@ -82,79 +82,79 @@ const ENTITY_DONPHAN = "donphan";
 
 // pokemon answers
 
-const SENTENCE_PIKACHU = "pika pika ! Unfortunally pikachu is road tripping around the world with Ash and can not be there, let's try one more time !";
-const SENTENCE_PICHU = "Pichu has been grounded and is actually having a bath with his mom and so it's not the expected answer, let's play again !";
-const SENTENCE_RAICHU = "raichu can not talk because he is providing electricity to his little brother to warm his bath, so let's try again !";
+const SENTENCE_PIKACHU = `<speak><sub alias="peeka peeka!">Pika Pika!</sub> Unfortunally Pikachu is road tripping around the world with Ash and can not be there, let's try one more time!</speak>`
+const SENTENCE_PICHU = "Pichu has been grounded and is actually having a bath with his mom and so it's not the expected answer, let's try again!";
+const SENTENCE_RAICHU = "Raichu can not talk because he is providing electricity to his little brother to warm his bath, so let's try again!";
 
-const SENTENCE_RATTATA = "rattata is dreaming of trying to fly with his winged friend instead of collecting raspberry for his father. So no, it is not him !";
-const SENTENCE_RATICATE = "Raticate is in the kitchen awaiting for raspberry, trying to create a new dishes to his wife for their wedding anniversary, so it's not him !";
+const SENTENCE_RATTATA = "Rattata is dreaming of trying to fly with his winged friend instead of collecting raspberries for his father. So no, it is not him!";
+const SENTENCE_RATICATE = "Raticate is in the kitchen awaiting for raspberry, trying to create a new dish for his wife for their wedding anniversary, so it's not him!";
 
-const SENTENCE_MARILL = "marill dabbles with his brothers and sisters in the neighboring lake in secret instead of doing his homework... so it can not be him !";
-const SENTENCE_AZUMARILL = "Azumarill is seeking his children, wondering where they are instead of doing theirs homeworks. try an other answer !";
+const SENTENCE_MARILL = "Marill dabbles with his brothers and sisters in the neighboring lake in secret instead of doing his homework... so it can not be him!";
+const SENTENCE_AZUMARILL = "Azumarill is seeking his children, wondering where they are instead of doing their homeworks. try an other answer!";
 
-const SENTENCE_MAGIKARP = "Magikarp, my favorite useless pokemon of the world... but now he is too busy to find water to scream. try another answer !";
+const SENTENCE_MAGIKARP = "Magikarp, my favorite useless pokemon of the world... Right now he's looking for a pond to swim, so this is not his cry. Try another answer!";
 
-const SENTENCE_BULBASAUR = "Bulbasaur is gardening at the bottom of a garden with a friend and it seems that something goes wrong. try another answer !";
-const SENTENCE_SQUIRTLE = "Squirtle is not here, he has joined the Squirtle Squad and used to run wild. He is probably writing graffiti and and swaggering with his bro at this time. let's try again !";
-const SENTENCE_CHARMANDER = "Charmander is with his friend at the bottom of the garden... it smells that they are starting a barbecue or something like that, so let's try another answer !";
-const SENTENCE_CHARIZARD = "Right, he is actually screaming at the bottom of the garden, I don't know why. But Charizard doesn't sounds like that, so let's try again !";
+const SENTENCE_BULBASAUR = "Bulbasaur is gardening at the bottom of a garden with a friend and it seems that something goes wrong. try another answer!";
+const SENTENCE_SQUIRTLE = "Squirtle is not here, he has joined the Squirtle Squad. He is probably writing graffiti and swaggering with his bros at this time. let's try again!";
+const SENTENCE_CHARMANDER = "Charmander is with his friend at the bottom of the garden... it seems that they are starting a barbecue or something like that, so let's try another answer!";
+const SENTENCE_CHARIZARD = "Right, he is actually screaming at the bottom of the garden, I don't know why. But Charizard doesn't sound like that, so let's try again!";
 
-const SENTENCE_PIDGEY = "Pidgey is trying desperately to awake his friend because if he doesn't bring raspberry to his daddy he will be grounded, so let's get another shot !";
-const SENTENCE_PIDGEOTTO = "pidgetto is having a laught watching his little neighbours set a fire to a wheelbarrow, so let's try again !";
-const SENTENCE_PIDGEOT = "pidgeot flew into his neighbor's garden for a wheelbarrow story... I don't know exactly but it can not be him, so let's play again !";
+const SENTENCE_PIDGEY = "Pidgey is trying desperately to awake his friend because if he doesn't bring raspberries to his daddy he will be grounded, so let's try another shot!";
+const SENTENCE_PIDGEOTTO = "Pidgetto is having a laught watching his little neighbours set a fire to a wheelbarrow, so let's try again!";
+const SENTENCE_PIDGEOT = "Pidgeot flew into his neighbor's garden for a wheelbarrow story... I don't know exactly but it can not be him, so let's try again!";
 
-const SENTENCE_PSYDUCK = "psyduck is missing, he escaped again from his owner and surely is trying to learn how to swim somewhere we don't know where, so let's try another answer !";
-const SENTENCE_GOLDUCK = "golduck is trying to talk to come back to psyduck through telepathy but it looks like that psyduck gave him a headache, so let's try again !";
-const SENTENCE_MURKROW = "Murkrow is actually grounded in his room, he tried to robbered a jewelry. This little boy has never been able to resist a brilliant object, so it is not him !";
+const SENTENCE_PSYDUCK = "Psyduck is missing, he escaped again from his owner and surely is trying to learn how to swim somewhere we don't know where, so let's try another answer!";
+const SENTENCE_GOLDUCK = "Golduck is trying to talk to come back to psyduck through telepathy but it looks like that psyduck gave him a headache, so let's try again!";
+const SENTENCE_MURKROW = "Murkrow is actually grounded in his room, he tried to rob a jewelry. This little boy has never been able to resist a brilliant object, so it is not him!";
 
-const SENTENCE_HOOTHOOT = "Hoothoot has embarked on the music, he is currently rehearsing in his room. let's try again !";
-const SENTENCE_NOCTOWL = "Noctowl listens to hoothoot music through the door and is too busy to wonders if he should encourage or stop him. so let's play again !";
+const SENTENCE_HOOTHOOT = "Hoothoot has embarked on the music, he is currently rehearsing in his room. let's try again!";
+const SENTENCE_NOCTOWL = "Noctowl listens to hoothoot music through the door and is too busy wondering if he should encourage or stop him. so let's try again!";
 
-const SENTENCE_ARTICUNO = "articuno has gone for a tour around the world with his two best friends to the delight of their fan so let's try again !";
-const SENTENCE_MOLTRES = "moltres has gone for a tour around the world with his two best friends to the delight of their fan, so it is not the correct answer";
-const SENTENCE_ZAPDOS = "zapdos has gone for a tour around the world with his two best friends to the delight of their fan so let's play again !";
+const SENTENCE_ARTICUNO = "Articuno has gone on tour around the world with his two best friends to the delight of their fans, so let's try again!";
+const SENTENCE_MOLTRES = "Moltres has gone on tour around the world with his two best friends to the delight of their fasn, so it is not the correct answer";
+const SENTENCE_ZAPDOS = "Zapdos has gone on tour around the world with his two best friends to the delight of their fans, so let's try again!";
 
-const SENTENCE_MEOWTH = "Meowth is in holiday camp trying to make new friend with 2 humans and other pokemon, let's try one more time ! ";
-const SENTENCE_PERSIAN = "persian is enjoying his free time without no one to bothered him, so it is not the good answer, let's try again !";
-const SENTENCE_MEW = "mew is daydreaming somewhere beetween water and air, we don't know when he is coming back home so it is not him, let's play again !";
-const SENTENCE_MEWTWO = "mewtwo is chasing after mew because mew is the only one who can calm his fierce heart, by bringing him to his origin, so let's try another one !";
+const SENTENCE_MEOWTH = "Meowth is in a holiday camp trying to make friends with two humans and other pokemon, let's try one more time! ";
+const SENTENCE_PERSIAN = "Persian is enjoying his free time without no one to bother him, so it is not the good answer, let's try again!";
+const SENTENCE_MEW = "Mew is daydreaming somewhere beetween water and air, we don't know when he is coming back home, so it is not him, let's try again!";
+const SENTENCE_MEWTWO = "Mewtwo is chasing after mew because mew is the only one who can calm his fierce heart, by bringing him back to himself, so let's try another one!";
 
-const SENTENCE_GROWLITHE = "growlithe has gone hiking for the first time with his brother and his friends, so it can not be him, let's try again !";
-const SENTENCE_ARCANINE = "Arcanine is leading a hike with friend and family all around the world for months, so let's play again !";
-const SENTENCE_SNUBULL = "Snubbull is shopping with his trainer to find a new little dress so it is not the good answer, let's try again";
-const SENTENCE_HOUNDOUR = "Houndour is gone for a hike with friend for a long time, he packed half the house afraid of missing something, so it's not the right answer, let's play again !";
-const SENTENCE_HOUNDOOM = "houndoom has gone with friends for a hike, afraid that his little brother missed something in his package and panic for nothing, so let's try another answer !";
+const SENTENCE_GROWLITHE = "Growlithe has gone hiking for the first time with his brother and his friends, so it can not be him, let's try again!";
+const SENTENCE_ARCANINE = "Arcanine is leading a hike with friends and family all around the world for months, so let's try again!";
+const SENTENCE_SNUBULL = "Snubbull is shopping with his trainer to find a new little dress, so it is not the good answer, let's try again";
+const SENTENCE_HOUNDOUR = "Houndour is gone for a hike with friend for a long time, he packed half the house afraid of missing something, so it's not the right answer, let's try again!";
+const SENTENCE_HOUNDOOM = "Houndoom has gone with friends for a hike, afraid that his little brother missed something in his package and panicked for nothing, so let's try another answer!";
 
-const SENTENCE_PONYTA = "Ponyta is running in fields and mountains all day, trying to be as fast as his big brother ! so it's not to good answer let's try again !";
-const SENTENCE_RAPIDASH = "rapidash is training his little brother to go faster, as he can run faster than the storm, so it cannot be him let's play again !";
+const SENTENCE_PONYTA = "Ponyta is running in fields and mountains all day, trying to be as fast as his big brother! It's not to good answer let's try again!";
+const SENTENCE_RAPIDASH = "Rapidash is training his little brother to go faster, as he can run faster than the storm, so it cannot be him let's try again!";
 
-const SENTENCE_EEVEE = "Eevee is outside, trying to find his own personnality by finding what he likes or need with a help of a friend, so let's try again !";
-const SENTENCE_VULPIX = "Vulpix is outside, trying to convince a friend to become like him, so they can be brother of fire, so let's try again !";
+const SENTENCE_EEVEE = "Eevee is outside, trying to find his own personality by finding what he likes with the help of a friend, so let's try again!";
+const SENTENCE_VULPIX = "Vulpix is outside, trying to convince a friend to become like him, so they can be brother of fire, so let's try again!";
 
-const SENTENCE_MAREEP = "Mareep is watching his big brother having his first shaving wool... he is too impressed to scream so let's try again !";
-const SENTENCE_FLAAFY = "flaafy is having his first lost of wool, trying to negociate to not loose everything, so it is not him let's play again !";
-const SENTENCE_AMPHAROS = "ampharos is too busy to shave his little brother and hold on to send signals across the ocean in the right way, so let's try one more time !";
+const SENTENCE_MAREEP = "Mareep is watching his big brother having his first shearing... He is too impressed to cry so let's try again!";
+const SENTENCE_FLAAFY = "Flaafy is having his first lost of wool, trying to negociate to not lose everything, so it is not him. let's try again!";
+const SENTENCE_AMPHAROS = "Ampharos is too busy shearing his little brother and paused sending signals across the ocean in the right way, so let's try one more time!";
 
-const SENTENCE_AIPOM = "Aipom is a monkey-like Pokémon with a three-fingered hand at the end of its tail. The tail is strong enough to anchor Aipom to a branch and suspend the rest of its body in the air, but also delicate enough to pick fruit and manipulate objects";
-const SENTENCE_MANKEY = "Mankey is trying to peel a chestnut but I can hear him getting nervous, so let's try another answer !";
+const SENTENCE_AIPOM = "Aipom is practicing peeling pomegranates so he can eat more at the same time! It's not him, try again!";
+const SENTENCE_MANKEY = "Mankey is trying to peel a chestnut but I can hear him getting nervous, so let's try another answer!";
 const SENTENCE_PRIMEAPE = "When Primeape is furious, its blood circulation is boosted. This makes its muscles stronger, but at the cost of some of its intelligence. It has shown some fondness for bananas.";
 
-const SENTENCE_EKANS = "ekans is in holiday camp, making friend with other pokemon and some humans, apparently they are starting a band, so let's try one more time !";
-const SENTENCE_ARBOK = "Arbok is too territorial, he went to look for hikers who would walk on his land, so it is not the right answer let's play again !";
+const SENTENCE_EKANS = "Ekans is in holiday camp, making friends with other pokemon and some humans, apparently they are starting a band, so let's try one more time!";
+const SENTENCE_ARBOK = "Arbok is too territorial, he went to look for hikers who would walk on his land, so it is not the right answer let's try again!";
 
-const SENTENCE_TAUROS = "Tauros trains to be faster and more efficient with its 3 tails, so it's not him, let's try again !";
+const SENTENCE_TAUROS = "Tauros trains to be faster and more efficient with its three tails, so it's not him, let's try again!";
 
-const SENTENCE_POLITOED = "politoed has joined the King's Rock band and sing in a chorus with a loud, bellowing voice at the end of the world so it can not be him, let's try again !";
+const SENTENCE_POLITOED = "Politoed has joined the King's Rock band and sings with a loud, bellowing voice at the other side of the planet, so it can not be him, let's try again!";
 
-const SENTENCE_GIRAFARIG = "Girafarig's tail's head is busy to calm down Mankey while Girafarig real head is picking bananas for the same reason, so let's try another one !";
+const SENTENCE_GIRAFARIG = "Girafarig's tail's head is busy calming down Mankey while Girafarig real head is picking chestnuts for the same reason, so let's try another one!";
 
-const SENTENCE_TEDDIURSA = "Teddiursa is using Beedrill pollen and fruit to create its own honey for not becoming scarce this winter. it's not the correct answer let's try again !";
-const SENTENCE_URSARING = "Ursaring is helping Teddiursa making honey for winter, so it's not the good answer, let's play again !";
+const SENTENCE_TEDDIURSA = "Teddiursa is using Beedrill pollen and fruit to create its own honey so it is not scarce this winter. It's not the correct answer let's try again!";
+const SENTENCE_URSARING = "Ursaring is helping Teddiursa make honey for the winter, so it's not the good answer, let's try again!";
 
-const SENTENCE_SWINUB = "Swinub is gormandize somewhere in the wood with his favorite mushroom that grows beneath dead grass, so it's not the correct answer, let's try again !";
+const SENTENCE_SWINUB = "Swinub is gormandizing somewhere in the wood with his favorite mushroom that grow beneath dead grass, so it's not the correct answer, let's try again!";
 
-const SENTENCE_PHANPY = "phanpy waddles in the river playing with water and his trunk to the great displeasure of his papa, so let's try one more time !";
-const SENTENCE_DONPHAN = "donphan is trying to teach his son to curl up into a ball which seems more likely to play than learn, so let's try again !";
+const SENTENCE_PHANPY = "Phanpy waddles in the river playing with water and his trunk to the great displeasure of his papa, so let's try one more time!";
+const SENTENCE_DONPHAN = "Donphan is trying to teach his son to curl up into a ball which seems more likely to play than learn, so let's try again!";
 
 class Pokemon extends SimpleIntent {
 
